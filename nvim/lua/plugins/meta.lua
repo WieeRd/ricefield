817906ffc910efc70948252fc33821b8331f6528
WieeRd <verywieerd@gmail.com>
2024-06-20 16:47:45 +0900

    feat(nvim): add nvim-treesitter

--- nvim/lua/plugins/meta.lua
+++ nvim/lua/plugins/meta.lua
@@ -22,4 +22,23 @@ return {
       })
     end,
   },
+
+  -- install and manage tree-sitter parsers and modules
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "vim",
+        "vimdoc",
+      },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
 }
