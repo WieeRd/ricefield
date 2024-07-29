a93dc6d729ffda2428ae826dd3a0686e1c452ff4
WieeRd <verywieerd@gmail.com>
2024-07-30 00:28:55 +0900

    refactor(nvim): move config loaders to a separate module

--- .config/nvim/lua/setup/init.lua
+++ .config/nvim/lua/setup/init.lua
@@ -1,13 +1,11 @@
 local vim = vim
 local M = {}
 
--- FEAT: add loaders - signs, lsp(diagnostics, keymaps)
--- FEAT: add unloaders
+-- FEAT: add loaders - lsp(diagnostics, keymaps)
 -- FEAT: add management commands
 -- FEAT: keep both original and evaluated config table
--- NOTE: autolsp.nvim
 
-function M.tbl_or_mod(target)
+local function tbl_or_mod(target)
   if type(target) == "table" then
     return target
   else
@@ -16,130 +14,32 @@ function M.tbl_or_mod(target)
   end
 end
 
-function M.bool_or_func(target)
-  if type(target) == "function" then
-    return target()
-  else
-    return target
-  end
-end
-
-function M.load_globals(globals)
-  local g = vim.g
-  for name, value in pairs(globals) do
-    g[name] = value
-  end
-end
-
-function M.load_options(options)
-  local opt = vim.opt
-  for name, value in pairs(options) do
-    opt[name] = value
-  end
-end
-
-function M.load_keymaps(keymaps)
-  local map = vim.keymap.set
-
-  for mode, mappings in pairs(keymaps) do
-    for lhs, rhs_and_opts in pairs(mappings) do
-      local rhs, opts
-
-      if type(rhs_and_opts) == "table" then
-        rhs = rhs_and_opts[1]
-        rhs_and_opts[1] = nil
-        opts = rhs_and_opts
-      else
-        rhs = rhs_and_opts
-        opts = {}
-      end
-
-      map(mode, lhs, rhs, opts)
-    end
-  end
-end
-
-function M.load_autocmds(autocmds)
-  local autocmd = vim.api.nvim_create_autocmd
-  local augroup = vim.api.nvim_create_augroup
-  local group = augroup("cfg", { clear = true })
-
-  for i = 1, #autocmds do
-    local opts = autocmds[i]
-    local event = opts[1]
-    opts[1] = nil
-    opts.group = opts.group or group
-
-    autocmd(event, opts)
-  end
-end
-
-function M.load_commands(commands)
-  local command = vim.api.nvim_create_user_command
-
-  for name, cmd_and_opts in pairs(commands) do
-    local cmd, opts
-
-    if type(cmd_and_opts) == "table" then
-      cmd = cmd_and_opts[1]
-      cmd_and_opts[1] = nil
-      opts = cmd_and_opts
-    else
-      cmd = cmd_and_opts
-      opts = {}
-    end
-
-    command(name, cmd, opts)
-  end
-end
-
-function M.load_signs(signs)
-  local sign_define = vim.fn.sign_define
-  for name, attr in pairs(signs) do
-    sign_define(name, attr)
-  end
-end
-
-function M.load_plugins(plugins)
-  if not M.bool_or_func(plugins.enabled) then
-    return false
-  end
-
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-
-  if not vim.uv.fs_stat(lazypath) then
-    vim.notify("Bootstrapping the plugin manager...")
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      lazyrepo,
-      lazypath,
-    })
-  end
-
-  vim.opt.runtimepath:prepend(lazypath)
-  require("lazy").setup(plugins.spec, plugins.opts)
-
-  return true
-end
-
 function M.setup(cfg)
-  cfg = M.tbl_or_mod(cfg or {})
+  local loaders = require("setup.loaders")
+  local modules = {
+    "globals",
+    "options",
+    "keymaps",
+    "autocmds",
+    "commands",
+    "signs",
+  }
 
-  M.load_globals(M.tbl_or_mod(cfg.globals or {}))
-  M.load_options(M.tbl_or_mod(cfg.options or {}))
-  M.load_keymaps(M.tbl_or_mod(cfg.keymaps or {}))
-  M.load_autocmds(M.tbl_or_mod(cfg.autocmds or {}))
-  M.load_commands(M.tbl_or_mod(cfg.commands or {}))
-  M.load_signs(M.tbl_or_mod(cfg.signs or {}))
+  cfg = tbl_or_mod(cfg or {})
+  for _, mod in ipairs(modules) do
+    loaders[mod](tbl_or_mod(cfg[mod] or {}))
+  end
 
-  if M.load_plugins(M.tbl_or_mod(cfg.plugins or {})) then
+  if loaders.plugins(tbl_or_mod(cfg.plugins or {})) then
     vim.cmd.colorscheme(cfg.colorscheme.plugin or "default")
   else
     vim.cmd.colorscheme(cfg.colorscheme.builtin or "default")
   end
+end
+
+function M.deactivate()
+  -- local unloaders = require("setup.unloaders")
+  -- FEAT: add unloaders
 end
 
 return M
