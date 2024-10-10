49019e55291808424ba2bcdda63a976ea97e5d3c
WieeRd <verywieerd@gmail.com>
2024-10-11 06:02:25 +0900

    feat(qtile): WM keymap revamp draft #1

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -10,11 +10,14 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 
 mod = "mod4"
-terminal = "kitty"
+
+TERMINAL = "kitty"
+BROWSER = "vivaldi"
 
 screens = [Screen()]  # FEAT: come up with a sensible dual monitor workflow
-groups = [Group(i) for i in "asdf1234"]
+groups = [Group(i) for i in "asdf1234"]  # FEAT: add scratchpad groups
 
+# FIX: fitting border colors for the kanagawa theme.
 layouts = [
     layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=2),
     layout.Max(),
@@ -26,55 +29,52 @@ mouse = [
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
-# FEAT: use KeyChord to imitate modal keybinds
+# FIX: use EzKey helper
 keys = [
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "p", lazy.layout.next(), desc="Move window focus to other window"),
+    # Launch programs
+    Key([mod], "z", lazy.spawn(TERMINAL), desc="Launch terminal"),
+    Key([mod], "x", lazy.spawn(BROWSER), desc="Launch browser"),
+    Key([mod], "c", lazy.spawn("rofi -show run"), desc="Launch command"),
+    Key([mod], "v", lazy.spawn("rofi -show drun"), desc="Launch application"),
 
-    # Move windows
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Switch between windows
+    # FEAT: goto last visited group
+    Key([mod], "h", lazy.layout.left(), desc="Focus left"),
+    Key([mod], "j", lazy.layout.down(), desc="Focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Focus up"),
+    Key([mod], "l", lazy.layout.right(), desc="Focus right"),
+    Key([mod], "space", lazy.spawn("rofi -show window"), desc="Browse windows"),
+
+    # Rearrange windows
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move left"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move up"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move right"),
+    Key([mod], "semicolon", lazy.layout.swap_column_left(), desc="Swap column"),
 
     # Resize windows
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow left"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow up"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow right"),
     Key([mod], "equal", lazy.layout.normalize(), desc="Reset all window sizes"),
 
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key(
-        [mod],
-        "f",
-        lazy.window.toggle_fullscreen(),
-        desc="Toggle fullscreen on the focused window",
-    ),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    # Manage windows
+    Key([mod], "w", lazy.window.kill(), desc="Close window"),
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating"),
+    Key([mod], "Tab", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
+
+    # Manage layouts
+    Key([mod], "o", lazy.layout.toggle_split(), desc="Toggle stacks"),
+    Key([mod, "shift"], "o", lazy.next_layout(), desc="Toggle layouts"),
+
+    # Manage desktop
+    # FEAT: shutdown, lock, reboot, restart
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawn("rofi -show drun"), desc="Spawn a command using a prompt widget"),
-    # Key([], "Super_L", lazy.spawn("rofi -show drun"), desc="Spawn a command using a prompt widget"),
 ]
 
+# REFACTOR: use list comprehension
 for i in groups:
     keys.extend(
         [
