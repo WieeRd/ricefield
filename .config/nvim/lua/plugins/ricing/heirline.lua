a8372b1f087baf386988ab5bb9fead0b2c01a412
WieeRd <verywieerd@gmail.com>
2024-12-06 21:15:33 +0900

    feat(nvim): reasonable file statusline

--- .config/nvim/lua/plugins/ricing/heirline.lua
+++ .config/nvim/lua/plugins/ricing/heirline.lua
@@ -4,7 +4,9 @@ local utils = require("heirline.utils")
 
 local function get_palette()
   local hl = utils.get_highlight
-  return {}
+  return {
+    Special = hl("Special").fg,
+  }
 end
 
 -- update the palette on colorscheme changes
@@ -21,8 +23,79 @@ local Align = { provider = "%=" }
 local Truncate = { provider = "%<" }
 local Space = { provider = " " }
 
+-- `128L 64C  32% ` | `  1L  2C   3% `
+-- Current line, column and percentage through file
+local Ruler = {
+  provider = "%3lL %2cC %3p%% ",
+  hl = { bold = true },
+}
+
+-- `[protocol]`
+-- Extract `protocol://` from URL-like buffer names
+local FileProtocol = {
+  condition = function(self)
+    return self.protocol
+  end,
+  provider = function(self)
+    return ("[%s]"):format(self.protocol)
+  end,
+  hl = { fg = "Special", bold = true },
+}
+
+-- `  `
+-- Filetype icon from Devicon
+local FileIcon = {
+  init = function(self)
+    if self.path:match("/$") then
+      self.icon = ""
+      self.highlight = "Directory"
+    else
+      local devicons = require("nvim-web-devicons")
+      self.icon, self.highlight =
+        devicons.get_icon_by_filetype(vim.bo.filetype, { default = true })
+    end
+  end,
+  provider = function(self)
+    return (" %s "):format(self.icon)
+  end,
+  hl = function(self)
+    return self.highlight
+  end,
+}
+
+-- `/etc/fstab` | `~/.profile` | `src/main.rs`
+-- Shortened path relative to $HOME and $PWD
+local FilePath = {
+  provider = function(self)
+    return vim.fn.fnamemodify(self.path, ":~:.")
+  end,
+}
+
+-- `[protocol]  src/main.rs ... 128L 64C  32% `
+-- Generic statusline ready for normal files as well as most special buffers
 local File = {
-  provider = "%f",
+  init = function(self)
+    self.protocol = nil
+    self.path = vim.api.nvim_buf_get_name(0)
+
+    if self.path == "" then
+      self.path = "[No Name]"
+      return
+    end
+
+    local protocol, name = self.path:match("(.-)://(.-/?)/?$")
+    if protocol then
+      self.protocol = protocol
+      self.path = name
+    end
+  end,
+
+  FileProtocol,
+  FileIcon,
+  FilePath,
+  Align,
+  Ruler,
+  Truncate,
 }
 
 local StatusLine = {
@@ -30,6 +103,8 @@ local StatusLine = {
     return cond.is_active() and "StatusLine" or "StatusLineNC"
   end,
 
+  -- use the first component that matches the condition
+  fallthrough = false,
   File,
 }
 
